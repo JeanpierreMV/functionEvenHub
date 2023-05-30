739dac89a9d3d7cbfe162c6d15e7bdadb0b2a32b
JeanpierreMV <jeanpierre_montalvo@usmp.pe>
2023-05-30 15:01:53 -0500

    first home

--- IoTHubtoTwins.cs
+++ IoTHubtoTwins.cs
@@ -10,21 +10,25 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Azure.Messaging.EventGrid;
+using System.Threading.Tasks;
+
 
 namespace IotHubtoTwins
 {
     public class IoTHubtoTwins
     {
-        private static readonly string adtInstanceUrl = Environment.GetEnvironmentVariable("ADT_SERVICE_URL");
+       private static readonly string adtInstanceUrl = "https://AzureDT-01.api.eus.digitaltwins.azure.net";
         private static readonly HttpClient httpClient = new HttpClient();
+
 
         [FunctionName("IoTHubtoTwins")]
         // While async void should generally be used with caution, it's not uncommon for Azure function apps, since the function app isn't awaiting the task.
 #pragma warning disable AZF0001 // Suppress async void error
-        public async void Run([EventGridTrigger] EventGridEvent eventGridEvent, ILogger log)
+        public async Task Run([EventGridTrigger] EventGridEvent eventGridEvent, ILogger log)
 #pragma warning restore AZF0001 // Suppress async void error
         {
             if (adtInstanceUrl == null) log.LogError("Application setting \"ADT_SERVICE_URL\" not set");
+
 
             try
             {
@@ -32,10 +36,11 @@ namespace IotHubtoTwins
                 var cred = new DefaultAzureCredential();
                 var client = new DigitalTwinsClient(new Uri(adtInstanceUrl), cred);
                 log.LogInformation($"ADT service client connection created.");
-            
+           
                 if (eventGridEvent != null && eventGridEvent.Data != null)
                 {
                     log.LogInformation(eventGridEvent.Data.ToString());
+
 
                     // <Find_device_ID_and_temperature>
                     JObject deviceMessage = (JObject)JsonConvert.DeserializeObject(eventGridEvent.Data.ToString());
@@ -43,7 +48,9 @@ namespace IotHubtoTwins
                     var temperature = deviceMessage["body"]["Temperature"];
                     // </Find_device_ID_and_temperature>
 
+
                     log.LogInformation($"Device:{deviceId} Temperature is:{temperature}");
+
 
                     // <Update_twin_with_device_temperature>
                     var updateTwinData = new JsonPatchDocument();
